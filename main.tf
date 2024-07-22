3770798fd57aef64ebef3df09fc9864dd1032899
Stooms21 <touzet.stan@protonmail.com>
2024-07-22 08:12:18 +0200

    Allez

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 	region = "us-west-2"
 }
 resource "aws_instance" "foo" {
-	ami = "ami-05fa00d4c63e32376" # us-west-2
+	ami = "ami-0b995c42184e99f98" # us-west-2
 	instance_type = "t2.micro"
 	tags = {
 	Name = "TF-Instance"
